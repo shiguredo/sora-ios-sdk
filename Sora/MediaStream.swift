d1fa118cf1e28353ef928e98ff99d016660ba4cd
akisute <akisutesama@gmail.com>
2017-09-28 18:45:38 +0900

    MediaStreamの修正
    - videoCapturerのwillSetにちょっと注釈
      - ここで本当はvideoCapture.stop()呼び出そうかと考えたんですが、そうすると一つのcapturerを複数のstreamにあてがう時に問題になると考えてやめました。
    - videoRendererAdapterをprivate letにしました
    - render時にCapturerが用意されていない場合、ダミーのCapturerを使用するようにしてみました。
      - 大丈夫じゃなかったらすみません(´･_･`)
      - とはいえこのあとの修正でcustom capturerを設定できるようにするので、そうするとcamera capturer以外のcapturerも出てくるのでどっちにしろ同じ問題が発生するはず。

--- Sora/MediaStream.swift
+++ Sora/MediaStream.swift
@@ -9,7 +9,7 @@ public protocol MediaStream: class {
     var videoFilter: VideoFilter? { get set }
     var videoRenderer: VideoRenderer? { get set }
     // var audioCapturer: AudioCapturer? { get set }
- 
+    
     // VideoCapturer から呼ばれる
     func render(videoFrame: VideoFrame?)
     
@@ -24,21 +24,26 @@ public class BasicMediaStream: MediaStream {
     
     public var videoCapturer: VideoCapturer? {
         willSet {
-            videoCapturer?.stream = nil
-            var newValue = newValue
-            newValue?.stream = self
+            if var oldValue = videoCapturer {
+                // Do not autostop here, let others manage videoCapturer's life cycle
+                oldValue.stream = nil
+            }
+            if var newValue = newValue {
+                newValue.stream = self
+                // Do not autostart here, let others manage videoCapturer's life cycle
+            }
         }
     }
     
     public var videoFilter: VideoFilter?
- 
-    var videoRendererAdapter: VideoRendererAdapter
+    
+    private let videoRendererAdapter: VideoRendererAdapter
     
     public var videoRenderer: VideoRenderer? {
         get { return videoRendererAdapter.videoRenderer }
         set { videoRendererAdapter.videoRenderer = newValue }
     }
-
+    
     public var nativeStream: RTCMediaStream
     
     public var nativeVideoTrack: RTCVideoTrack? {
@@ -48,7 +53,7 @@ public class BasicMediaStream: MediaStream {
     public var nativeVideoSource: RTCVideoSource? {
         get { return nativeVideoTrack?.source }
     }
-
+    
     public init(nativeStream: RTCMediaStream) {
         self.nativeStream = nativeStream
         streamId = nativeStream.streamId
@@ -57,6 +62,7 @@ public class BasicMediaStream: MediaStream {
         nativeVideoTrack?.add(videoRendererAdapter)
     }
     
+    private static let dummyCapturer: RTCVideoCapturer = RTCVideoCapturer()
     public func render(videoFrame: VideoFrame?) {
         if let frame = videoFrame {
             // フィルターを通す
@@ -64,9 +70,8 @@ public class BasicMediaStream: MediaStream {
             switch frame {
             case .native(capturer: let capturer, frame: let nativeFrame):
                 // RTCVideoSource.capturer(_:didCapture:) の最初の引数は
-                // 現在使われてないのでダミーでも可？
-                nativeVideoSource?.capturer(capturer ??
-                    CameraVideoCapturer.shared.nativeCameraVideoCapturer!,
+                // 現在使われてないのでダミーでも可？ -> ダミーにしました
+                nativeVideoSource?.capturer(capturer ?? BasicMediaStream.dummyCapturer,
                                             didCapture: nativeFrame)
                 
             default:
