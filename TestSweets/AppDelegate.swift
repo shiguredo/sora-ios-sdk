dd4ff52263246591db91321c67825c6c81cdf357
SUZUKI Tetsuya <szktty@shiguredo.jp>
2017-10-11 17:13:08 +0900

    終了処理

--- TestSweets/AppDelegate.swift
+++ TestSweets/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Sora
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -31,6 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        Sora.finish()
     }
 
 
