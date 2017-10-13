4a69b2b51d436907f00e542b4c04f6eff0eb172f
SUZUKI Tetsuya <szktty@shiguredo.jp>
2017-10-14 02:39:12 +0900

    ログレベルの設定

--- TestSweets/AppDelegate.swift
+++ TestSweets/AppDelegate.swift
@@ -10,6 +10,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        Sora.logLevel = .debug
+        
         Sora.shared.handlers.onConnectHandler = { chan, error in
             print("Sora.onConnect(\(String(describing: chan)), \(String(describing: error)))")
         }
