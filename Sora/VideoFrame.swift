347cb3cc8a54cb0949a9feeb18a938e455d41d2f
SUZUKI Tetsuya <szktty@shiguredo.jp>
2017-10-19 15:39:32 +0900

    ドキュメント

--- Sora/VideoFrame.swift
+++ Sora/VideoFrame.swift
@@ -2,21 +2,29 @@ import Foundation
 import CoreMedia
 import WebRTC
 
+/**
+ 映像フレームの種別です。
+ 現在の実装では次の映像フレームに対応しています。
+ 
+ - ネイティブの映像フレーム (`RTCVideoFrame`)
+ - スナップショット (`Snapshot`)
+ - `CMSampleBuffer` (`RTCVideoFrame` に変換されます)
+ 
+ */
 public enum VideoFrame {
     
-    public init?(from sampleBuffer: CMSampleBuffer) {
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-            return nil
-        }
-        let timeStamp = CMTimeGetSeconds(CMSampleBufferGetPresentationTimeStamp(sampleBuffer))
-        let timeStampNs = Int64(timeStamp * 1_000_000_000)
-        let frame = RTCVideoFrame(pixelBuffer: pixelBuffer, rotation: RTCVideoRotation._0, timeStampNs: timeStampNs)
-        self = .native(capturer: nil, frame: frame)
-    }
+    // MARK: - 定義
     
+    /// ネイティブの映像フレーム。
+    /// `CMSampleBuffer` から生成した映像フレームは、ネイティブの映像フレームに変換されます。
     case native(capturer: RTCVideoCapturer?, frame: RTCVideoFrame)
+    
+    /// スナップショット
     case snapshot(Snapshot)
     
+    // MARK: - プロパティ
+    
+    /// 映像フレームの幅
     public var width: Int {
         get {
             switch self {
@@ -28,6 +36,7 @@ public enum VideoFrame {
         }
     }
     
+    /// 映像フレームの高さ
     public var height: Int {
         get {
             switch self {
@@ -39,6 +48,7 @@ public enum VideoFrame {
         }
     }
 
+    /// 映像フレームの生成日時
     public var timestamp: CMTime? {
         get {
             switch self {
@@ -50,4 +60,24 @@ public enum VideoFrame {
         }
     }
 
+    
+    // MARK: - 初期化
+    
+    /**
+     初期化します。
+     指定されたサンプルバッファーからピクセル画像データを取得できなければ
+     `nil` を返します。
+     
+     - parameter sampleBuffer: ピクセルバッファーを含むサンプルバッファー
+     */
+    public init?(from sampleBuffer: CMSampleBuffer) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return nil
+        }
+        let timeStamp = CMTimeGetSeconds(CMSampleBufferGetPresentationTimeStamp(sampleBuffer))
+        let timeStampNs = Int64(timeStamp * 1_000_000_000)
+        let frame = RTCVideoFrame(pixelBuffer: pixelBuffer, rotation: RTCVideoRotation._0, timeStampNs: timeStampNs)
+        self = .native(capturer: nil, frame: frame)
+    }
+    
 }
