d8f998b8605a202e806dd9aedb636df6d94706ad
SUZUKI Tetsuya <szktty@shiguredo.jp>
2018-05-25 16:45:54 +0900

    notify のパラメーターを定義し直す

--- Sora/SignalingMessage.swift
+++ Sora/SignalingMessage.swift
@@ -124,23 +124,54 @@ public struct SignalingSnapshotMessage {
  */
 public struct SignalingNotifyMessage {
     
+    // MARK: イベント情報
+    
     /// イベントの種別
     public let eventType: SignalingNotificationEventType
     
+    // MARK: 接続情報
+    
     /// ロール
-    public let role: SignalingRole
+    public let role: SignalingRole?
+    
+    /// チャネル ID
+    public let channelId: String?
+    
+    /// クライアント ID
+    public let clientId: String?
+    
+    /// 音声の可否
+    public let audioEnabled: Bool?
+    
+    /// 映像の可否
+    public let videoEnabled: Bool?
+    
+    // MARK: 統計情報
     
     /// 接続時間
-    public let connectionTime: Int
+    public let connectionTime: Int?
     
     /// 接続中のクライアントの数
-    public let connectionCount: Int
+    public let connectionCount: Int?
     
     /// 接続中のパブリッシャーの数
-    public let publisherCount: Int
+    public let publisherCount: Int?
     
     /// 接続中のサブスクライバーの数
-    public let subscriberCount: Int
+    public let subscriberCount: Int?
+    
+    // MARK: スポットライト機能
+    
+    /// スポットライト ID
+    public let spotlightId: String?
+    
+    /// 固定の有無
+    public let isFixed: Bool?
+    
+    // MARK: メタデータ
+    
+    /// メタデータ
+    public private(set) var metadata: [Any]?
     
 }
 
@@ -186,6 +217,18 @@ public enum SignalingMessage {
     
     /// "pong" シグナリングメッセージ
     case pong
+    
+    static func decode(from data: Data) throws -> SignalingMessage {
+        let decoder = JSONDecoder()
+        let msg = try decoder.decode(SignalingMessage.self, from: data)
+        switch msg {
+        case .notify(message: var notify):
+            notify.parseMetadata(from: data)
+        default:
+            break
+        }
+        return msg
+    }
     
 }
 
@@ -397,18 +440,56 @@ extension SignalingNotifyMessage: Codable {
         case connectionCount = "channel_connections"
         case publisherCount = "channel_upstream_connections"
         case subscriberCount = "channel_downstream_connections"
+        case channelId = "channel_id"
+        case clientId = "client_id"
+        case spotlightId = "spotlight_id"
+        case audio = "audio"
+        case video = "video"
+        case fixed = "fixed"
+        case metadata = "metadata"
+        case metadataList = "metadata_list"
     }
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         eventType = SignalingNotificationEventType(rawValue:
             try container.decode(String.self, forKey: .eventType))!
-        role = SignalingRole(rawValue:
-            try container.decode(String.self, forKey: .role))!
-        connectionTime = try container.decode(Int.self, forKey: .connectionTime)
-        connectionCount = try container.decode(Int.self, forKey: .connectionCount)
-        publisherCount = try container.decode(Int.self, forKey: .publisherCount)
-        subscriberCount = try container.decode(Int.self, forKey: .subscriberCount)
+        
+        if let raw = try container.decodeIfPresent(String.self, forKey: .role) {
+            role = SignalingRole(rawValue: raw)
+        } else {
+            role = nil
+        }
+        
+        connectionTime = try container.decodeIfPresent(Int.self, forKey: .connectionTime)
+        connectionCount = try container.decodeIfPresent(Int.self, forKey: .connectionCount)
+        publisherCount = try container.decodeIfPresent(Int.self, forKey: .publisherCount)
+        subscriberCount = try container.decodeIfPresent(Int.self, forKey: .subscriberCount)
+        channelId = try container.decodeIfPresent(String.self, forKey: .channelId)
+        clientId = try container.decodeIfPresent(String.self, forKey: .clientId)
+        audioEnabled = try container.decodeIfPresent(Bool.self, forKey: .audio)
+        videoEnabled = try container.decodeIfPresent(Bool.self, forKey: .video)
+        spotlightId = try container.decodeIfPresent(String.self, forKey: .spotlightId)
+        isFixed = try container.decodeIfPresent(Bool.self, forKey: .fixed)
+        
+        // metadata には任意のデータが入るため、 Decoder ではデコードできない
+    }
+    
+    mutating func parseMetadata(from data: Data) {
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            if let msg = json as? [String: Any] {
+                if let metadata =
+                    msg[SignalingNotifyMessage.CodingKeys.metadata.rawValue] {
+                    self.metadata = [metadata]
+                } else if let metadataList =
+                    msg[SignalingNotifyMessage.CodingKeys.metadataList.rawValue] {
+                    self.metadata = metadataList as? [Any]
+                }
+            }
+        } catch {
+            // 何もしない
+        }
     }
     
     public func encode(to encoder: Encoder) throws {
