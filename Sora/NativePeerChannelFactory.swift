4f66e0c26ba0059e18d06b6c3a7b9ab11fda75e8
SUZUKI Tetsuya <szktty@shiguredo.jp>
2019-04-09 16:30:19 +0900

    映像コーデックの enc/dec を追加する

--- Sora/NativePeerChannelFactory.swift
+++ Sora/NativePeerChannelFactory.swift
@@ -9,7 +9,23 @@ class NativePeerChannelFactory {
     
     init() {
         Logger.debug(type: .peerChannel, message: "create native peer channel factory")
-        nativeFactory = RTCPeerConnectionFactory()
+        
+        // 映像コーデックのエンコーダーとデコーダーを用意する
+        // Sora iOS SDK では VP8, VP9, H.264 が有効
+        let encoder = RTCDefaultVideoEncoderFactory()
+        let decoder = RTCDefaultVideoDecoderFactory()
+        nativeFactory =
+            RTCPeerConnectionFactory(encoderFactory: encoder,
+                                     decoderFactory: decoder)
+        
+        for info in encoder.supportedCodecs() {
+            Logger.debug(type: .peerChannel,
+                         message: "supported video encoder: \(info.name) \(info.parameters)")
+        }
+        for info in decoder.supportedCodecs() {
+            Logger.debug(type: .peerChannel,
+                         message: "supported video decoder: \(info.name) \(info.parameters)")
+        }
     }
     
     func createNativePeerChannel(configuration: WebRTCConfiguration,
