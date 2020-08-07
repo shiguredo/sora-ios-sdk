41b6c1746de583869f55b0a59321cb9816cd1bd3
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-08-07 15:37:01 +0900

    不要なコードを消す

--- Sora/Statistics.swift
+++ Sora/Statistics.swift
@@ -1,36 +1,6 @@
 import Foundation
 import WebRTC
 
-private extension NSObject {
-    
-    var rtcStringValue: String? {
-        self as? String
-    }
-    
-    var rtcUint32Value: UInt32? {
-        (self as? NSNumber)?.uint32Value
-    }
-    
-    var rtcDoubleValue: Double? {
-        (self as? NSNumber)?.doubleValue
-    }
-    
-    var rtcListValue: [String]? {
-        guard let list = self as? NSArray else {
-            return nil
-        }
-        var values: [String] = []
-        for value in list {
-            guard let value = value as? String else {
-                return nil
-            }
-            values.append(value)
-        }
-        return values
-    }
-    
-}
-
 /// :nodoc:
 public class Statistics {
     
@@ -76,28 +46,3 @@ public class StatisticsEntry {
     }
     
 }
-
-/*
-extension Statistics: Encodable {
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.unkeyedContainer()
-        try container.encode(["a":"b"], forKey: .a)
-    }
-    
-}
-
-
-extension StatisticsEntry: Encodable {
-    
-    enum CodingKeys: CodingKey {
-        case a
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(["a":"b"], forKey: .a)
-    }
-    
-}
-*/
