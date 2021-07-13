c1373243634dc199e094e2443ca1f5e9ff31a5e5
SUZUKI Tetsuya <szktty@shiguredo.jp>
2021-07-13 16:09:35 +0900

    Package.swift を追加

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let file = "WebRTC-91.4472.9.1/WebRTC.xcframework.zip"
+
+let package = Package(
+    name: "Sora",
+    products: [
+        .library(name: "Sora", targets: ["Sora"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/daltoniam/Starscream.git", .exact( "3.1.1")),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/shiguredo/sora-ios-sdk-specs/releases/download/\(file)",
+            checksum: "1951c3e83259a28f594b0447565c6284cd6a281639492179799428e17e1da325"),
+        .target(
+            name: "Sora",
+            dependencies: ["Starscream"]),
+    ]
+)
