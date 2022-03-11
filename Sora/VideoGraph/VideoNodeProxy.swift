005a292ba982eff1f0599a26a467a34a51bdd98c
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2022-03-12 00:50:56 +0900

    主なメソッドを実装

--- Sora/VideoGraph/VideoNodeProxy.swift
+++ Sora/VideoGraph/VideoNodeProxy.swift
@@ -4,7 +4,34 @@ import WebRTC
 public class VideoNodeProxy: VideoNode {
     public weak var node: VideoNode?
 
-    override public init() {}
+    override public init() {
+        super.init()
+    }
+
+    public init(_ node: VideoNode) {
+        self.node = node
+        super.init()
+    }
+
+    override public func prepare() async {
+        await node?.prepare()
+    }
+
+    override public func start() async {
+        await node?.start()
+    }
+
+    override public func pause() async {
+        await node?.pause()
+    }
+
+    override public func stop() async {
+        await node?.stop()
+    }
+
+    override public func reset() async {
+        await node?.reset()
+    }
 
     override public func processFrameBuffer(_ buffer: VideoFrameBuffer?) async -> VideoFrameBuffer? {
         if let node = node {
