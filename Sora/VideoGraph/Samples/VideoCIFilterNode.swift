58905d29d0cfe6689fa7fc977d7b6f2bac83a414
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2022-03-12 16:10:45 +0900

    CIFilter を使うサンプルを用意する

---
+++ Sora/VideoGraph/Samples/VideoCIFilterNode.swift
@@ -0,0 +1,39 @@
+import Foundation
+import WebRTC
+
+// Core Image Filter を使うノードのサンプル
+public class VideoCIFilterNode: VideoNode {
+    public var filter: CIFilter?
+
+    override public init() {
+        super.init()
+    }
+
+    override public func processFrameBuffer(_ buffer: VideoFrameBuffer?) async -> VideoFrameBuffer? {
+        guard let buffer = buffer else {
+            return nil
+        }
+        guard let filter = filter else {
+            return buffer
+        }
+
+        // ここでは一番簡単なCore Imageを使ったフィルタリングを実装しています。
+        // 大本のビデオフレームバッファ (CMSampleBuffer) から画像フレームバッファ (CVPixelBuffer) を取りだし、
+        // Core ImageのCIImageに変換して、フィルタをかけます。
+        // 最後にフィルタリングされたCIImageをCIContext経由で元々の画像フレームバッファ領域に上書きレンダリングしています。
+        // 元々の画像フレームバッファ領域に直接上書きしているので、大本のビデオフレームバッファをそのまま引き続き使用することができ、
+        // 最終的にはこのビデオフレームバッファをSora SDKの提供するVideoFrameに変換して配信することができます。
+
+        guard let pixelBuffer = buffer.pixelBuffer else {
+            return buffer
+        }
+        let cameraImage = CIImage(cvPixelBuffer: pixelBuffer)
+        filter.setValue(cameraImage, forKey: kCIInputImageKey)
+        guard let filteredImage = filter.outputImage else {
+            return buffer
+        }
+        let context = CIContext(options: nil)
+        context.render(filteredImage, to: pixelBuffer)
+        return buffer
+    }
+}
