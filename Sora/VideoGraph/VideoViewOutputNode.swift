c89a5b35cb6d97950ab2b5df82d4eb2bd2cf5340
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2022-03-16 16:11:42 +0900

    start() 時に VideoView も start する

--- Sora/VideoGraph/VideoViewOutputNode.swift
+++ Sora/VideoGraph/VideoViewOutputNode.swift
@@ -14,6 +14,16 @@ public class VideoViewOutputNode: VideoOutputNode {
         videoView.start()
     }
 
+    override public func start() async {
+        await super.start()
+
+        if let videoView = videoView {
+            if await !videoView.isRendering {
+                await videoView.start()
+            }
+        }
+    }
+
     override public func processFrameBuffer(_ buffer: VideoFrameBuffer?, in context: VideoGraph.Context) async -> VideoFrameBuffer? {
         print("VideoViewOutputNode: frame \(buffer)")
         guard let videoView = videoView else {
