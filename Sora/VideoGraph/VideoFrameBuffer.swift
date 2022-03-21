269c91f9412f80bfe666d30a45fc7baf0d17ac55
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2022-03-21 17:39:01 +0900

    WIP: I420 -> CVPixelBuffer への変換

--- Sora/VideoGraph/VideoFrameBuffer.swift
+++ Sora/VideoGraph/VideoFrameBuffer.swift
@@ -26,10 +26,7 @@ public class VideoFrameBuffer {
                 if let rtcPixelBuffer = frame.buffer as? RTCCVPixelBuffer {
                     _pixelBuffer = rtcPixelBuffer.pixelBuffer
                 } else if let i420Buffer = frame.buffer as? RTCI420Buffer {
-                    // TODO: CVPixelBuffer に変換する
-                    return nil
-                } else {
-                    return nil
+                    _pixelBuffer = i420Buffer.pixelBuffer
                 }
             case let .sampleBuffer(buffer):
                 _pixelBuffer = CMSampleBufferGetImageBuffer(buffer)
@@ -72,3 +69,70 @@ public class VideoFrameBuffer {
 }
 
 public struct VideoFrameFormat {}
+
+private extension RTCI420Buffer {
+    var pixelBuffer: CVPixelBuffer? {
+        typealias DescriptorType = MemoryLayout<CVPlanarPixelBufferInfo_YCbCrPlanar>
+        let descriptor = UnsafeMutableRawPointer.allocate(byteCount: DescriptorType.size, alignment: DescriptorType.alignment)
+        defer {
+            descriptor.deallocate()
+        }
+
+        // I420 = 4
+        // YUV なら 3
+        let numberOfPlanes = 4
+
+        let baseAddrs = UnsafeMutablePointer<UnsafeMutableRawPointer?>.allocate(capacity: 3)
+        // TODO: サイズは？
+        let baseAddrY = UnsafeMutablePointer<UInt8>.allocate(capacity: 1000)
+        let baseAddrU = UnsafeMutablePointer<UInt8>.allocate(capacity: 1000)
+        let baseAddrV = UnsafeMutablePointer<UInt8>.allocate(capacity: 1000)
+        defer {
+            baseAddrs.deallocate()
+            baseAddrY.deallocate()
+            baseAddrU.deallocate()
+            baseAddrV.deallocate()
+        }
+        baseAddrs[0] = UnsafeMutableRawPointer(baseAddrY)
+        baseAddrs[1] = UnsafeMutableRawPointer(baseAddrU)
+        baseAddrs[2] = UnsafeMutableRawPointer(baseAddrV)
+
+        let planeWidths = UnsafeMutablePointer<Int>.allocate(capacity: 3)
+        let planeHeights = UnsafeMutablePointer<Int>.allocate(capacity: 3)
+        let planeBytesPerRow = UnsafeMutablePointer<Int>.allocate(capacity: 3)
+        defer {
+            planeWidths.deallocate()
+            planeHeights.deallocate()
+            planeBytesPerRow.deallocate()
+        }
+
+        planeWidths[0] = Int(width)
+        planeWidths[1] = Int(chromaWidth)
+        planeWidths[2] = Int(chromaWidth)
+        planeHeights[0] = Int(height)
+        planeHeights[1] = Int(chromaHeight)
+        planeHeights[2] = Int(chromaHeight)
+        planeBytesPerRow[0] = Int(strideY)
+        planeBytesPerRow[1] = Int(strideU)
+        planeBytesPerRow[2] = Int(strideV)
+
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreateWithPlanarBytes(nil,
+                                                        Int(width),
+                                                        Int(height),
+                                                        kCVPixelFormatType_420YpCbCr8BiPlanarFullRange,
+                                                        descriptor,
+                                                        0,
+                                                        numberOfPlanes,
+                                                        baseAddrs,
+                                                        planeWidths,
+                                                        planeHeights,
+                                                        planeBytesPerRow,
+                                                        nil,
+                                                        nil,
+                                                        nil,
+                                                        &pixelBuffer)
+        print("# status = \(status)")
+        return pixelBuffer!
+    }
+}
