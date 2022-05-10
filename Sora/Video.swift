ec95e10e751fd340549e5f78501103a1f39a8ff3
SUZUKI Tetsuya <szktty@shiguredo.jp>
2022-05-10 17:00:25 +0900

    SwiftUI 用 API の見直し

--- Sora/Video.swift
+++ Sora/Video.swift
@@ -4,33 +4,81 @@ import UIKit
 
 public struct Video<Background>: View where Background: View {
 
-    private var background: Background
+    var stream: Binding<MediaStream?>
+    var videoView: Binding<VideoView>?
+    var background: Background
 
-    @ObservedObject private var controller: VideoController
+    @ObservedObject var controller: VideoController
 
-    public init(_ controller: VideoController) where Background == EmptyView {
-        self.init(controller, background: {
-            EmptyView()
-        })
+    public init(_ stream: Binding<MediaStream?>,
+                videoView: Binding<VideoView>? = nil) where Background == EmptyView {
+        self.init(stream, background: EmptyView())
     }
 
-    public init(_ controller: VideoController,
-                background: () -> Background) {
-        self.controller = controller
-        self.background = background()
+    init(_ stream: Binding<MediaStream?>,
+         videoView: Binding<VideoView>? = nil,
+         background: Background) {
+        self.stream = stream
+        self.videoView = videoView
+        self.background = background
+        controller = VideoController(stream: stream)
+        videoView?.wrappedValue = controller.videoView
     }
 
     public var body: some View {
         ZStack {
             background
                 .opacity(controller.isCleared ? 1 : 0)
-            MainVideo(controller)
+            RepresentedVideoView(controller)
                 .opacity(controller.isCleared ? 0 : 1)
         }
     }
+
+    public func debugMode(_ flag: Bool) -> Video<Background> {
+        controller.videoView.debugMode = flag
+        return self
+    }
+
+    public func connectionMode(_ mode: VideoViewConnectionMode) -> Video<Background> {
+        controller.videoView.connectionMode = mode
+        return self
+    }
+
+    public func videoBackground<Background>(_ background: Background) -> Video<Background> where Background: View {
+        var new = Video<Background>(stream, background: background)
+        new.controller = controller
+        return new
+    }
+
+    public func videoStop(_ flag: Bool) -> Video<Background> {
+        if flag {
+            controller.videoView.stop()
+        } else if !controller.videoView.isRendering {
+            controller.videoView.start()
+        }
+        return self
+    }
+
+    public func videoClear(_ flag: Bool) -> Video<Background> {
+        if flag {
+            controller.videoView.clear()
+            controller.isCleared = true
+        }
+        return self
+    }
+
+    public func videoOnChange(perform: @escaping (CGSize) -> Void) -> Video<Background> {
+        controller.videoView.handlers.onChange = perform
+        return self
+    }
+
+    public func videoOnRender(perform: @escaping (VideoFrame?) -> Void) -> Video<Background> {
+        controller.videoView.handlers.onRender = perform
+        return self
+    }
 }
 
-fileprivate struct MainVideo: UIViewRepresentable {
+fileprivate struct RepresentedVideoView: UIViewRepresentable {
     typealias UIViewType = VideoView
 
     @ObservedObject private var controller: VideoController
@@ -40,60 +88,26 @@ fileprivate struct MainVideo: UIViewRepresentable {
     }
 
     public func makeUIView(context: Context) -> VideoView {
-        controller.videoView
+        return controller.videoView
     }
 
-    public func updateUIView(_ uiView: VideoView, context: Context) {}
+    public func updateUIView(_ uiView: VideoView, context: Context) {
+        controller.stream.wrappedValue?.videoRenderer = uiView
+    }
 
 }
 
-public class VideoController: ObservableObject {
-    public var stream: MediaStream? {
-        didSet {
-            stream?.videoRenderer = videoView
-        }
-    }
+class VideoController: ObservableObject {
 
-    public var connectionMode: VideoViewConnectionMode = .autoClear {
-        didSet {
-            videoView.connectionMode = connectionMode
-        }
-    }
+    var stream: Binding<MediaStream?>
 
-    public var debugMode: Bool = false {
-        didSet {
-            videoView.debugMode = debugMode
-        }
-    }
+    // init() で VideoView を生成すると次のエラーが出るので、生成のタイミングを遅らせておく
+    // Failed to bind EAGLDrawable: <CAEAGLLayer: 0x********> to GL_RENDERBUFFER 1
+    lazy var videoView = VideoView()
 
-    public var currentVideoFrameSize: CGSize? {
-        videoView.currentVideoFrameSize
-    }
+    @Published var isCleared: Bool = false
 
-    public internal(set) var videoView: VideoView
-
-    public private(set) var isRendering: Bool = true
-
-    @Published var isCleared = false
-
-    public init() {
-        videoView = VideoView()
-        videoView.start()
-    }
-
-    public func start() {
-        videoView.start()
-        isRendering = true
-        isCleared = false
-    }
-
-    public func stop() {
-        videoView.stop()
-        isRendering = false
-    }
-
-    public func clear() {
-        videoView.clear()
-        isCleared = true
+    init(stream: Binding<MediaStream?>) {
+        self.stream = stream
     }
 }
