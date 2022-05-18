595d6b636bcb37674cfbc2ed42209fd30704fe74
miosakuma <mio.sakuma.gml@gmail.com>
2022-05-18 15:20:35 +0900

    WebRTC 102.5005.7.1 に上げる

--- CHANGES.md
+++ CHANGES.md
@@ -11,6 +11,9 @@
 
 ## develop
 
+- [UPDATE] WebRTC 102.5005.7.1 に上げる
+    - @miosakuma
+
 ## 2022.2.1
 
 - [UPDATE] App Store Connect に bitcode を有効にしてバイナリをアップロードするとエラーになる問題の暫定回避策として、 WebRTC 97.4692.4.0 に下げる
--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import Foundation
 import PackageDescription
 
-let file = "WebRTC-97.4692.4.0/WebRTC.xcframework.zip"
+let file = "WebRTC-102.5005.7.1/WebRTC.xcframework.zip"
 
 let package = Package(
     name: "Sora",
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "WebRTC",
             url: "https://github.com/shiguredo/sora-ios-sdk-specs/releases/download/\(file)",
-            checksum: "f5bb3bc266f63616a4c608ca4dd5396bc084f2b8e99d4f7d756235e2e0d9afef"
+            checksum: "34521dad25d81c5dcc9450a9909efb25d585502f3cf3faf048a54967395780f3"
         ),
         .target(
             name: "Sora",
