f2dc7316e1cc1e4c139b6affad5476a5a6b3bb54
zztkm <zztkm@tsurutatakumi.info>
2025-01-18 20:50:43 +0900

    動いていた commit に戻す
     commit: 1b2811c84cc70130ac547f0a8cb7912b169fcb4c

--- Sora/SwiftUIVideoView.swift
+++ Sora/SwiftUIVideoView.swift
@@ -7,15 +7,25 @@ import UIKit
  */
 @available(iOS 14, *)
 public struct SwiftUIVideoView<Background>: View where Background: View {
-    @ObservedObject private var controller: VideoController
+    private var stream: MediaStream?
     private var background: Background
+
+    // TODO(zztkm): わかりやすいコメントを書く
+    // 親 View で定義された stopVideo 変数と接続するための変数
+    @Binding private var isStop: Bool
+    @Binding private var isClear: Bool
+
+    @ObservedObject private var controller: VideoController
+
     /**
      ビューを初期化します。
 
-     - parameter controller: VideoController
+     - parameter stream: 描画される映像ストリーム。 nil の場合は何も描画されません
+     - parameter isStop: 映像の描画を制御するフラグ (default: false)
+     - parameter isClear: 映像をクリアして背景 View を表示するためのフラグ (default: false)
      */
-    public init(_ controller: VideoController) where Background == EmptyView {
-        self.init(controller, background: EmptyView())
+    public init(_ stream: MediaStream?, isStop: Binding<Bool>? = nil, isClear: Binding<Bool>? = nil) where Background == EmptyView {
+        self.init(stream, background: EmptyView(), isStop: isStop, isClear: isClear)
     }
 
     /**
@@ -26,23 +36,22 @@ public struct SwiftUIVideoView<Background>: View where Background: View {
      - parameter isStop: 映像の描画を制御するフラグ (default: false)
      - parameter isClear: 映像をクリアして背景 View を表示するためのフラグ (default: false)
      */
-    public init(_ controller: VideoController, background: Background) {
-        self.controller = controller
+    public init(_ stream: MediaStream?, background: Background, isStop: Binding<Bool>? = nil, isClear: Binding<Bool>? = nil) {
+        self.stream = stream
         self.background = background
+        // 指定がない場合は固定値 false を与える
+        _isStop = isStop ?? .constant(false)
+        _isClear = isClear ?? .constant(false)
+        controller = VideoController(stream: stream)
     }
 
     /// :nodoc:
-    /// TODO(zztkm): SwiftUI での Background と UIKit での Background が同居しているが使い分けについて考える
-    /// 今の実装だと isClear で UIKit の方も clear してるけど、これは意味がなくて isClear すると SwiftUI の Backgroud View が表示されるようになる
-    /// なので、そもそも VideView の clear() method を呼ぶ意味がなくなってしまってる
-    /// この辺の制御はできるだけ SwiftUI 側に寄せたいので、clear() metthod を呼ばないようにするなど検討したい
     public var body: some View {
         ZStack {
-            // isClear が true のときは背景を表示し、false のときは Video を表示する
             background
-                .opacity(isClear ? 1 : 0)
+                .opacity(controller.isCleared ? 1 : 0)
             RepresentedVideoView(controller, isStop: $isStop, isClear: $isClear)
-                .opacity(isClear ? 0 : 1)
+                .opacity(controller.isCleared ? 0 : 1)
         }
     }
 
@@ -76,7 +85,33 @@ public struct SwiftUIVideoView<Background>: View where Background: View {
 
     /// 映像のクリア時に表示する背景ビューを指定します。
     public func videoBackground<Background>(_ background: Background) -> SwiftUIVideoView<Background> where Background: View {
-        SwiftUIVideoView<Background>(controller, background: background)
+        var new = SwiftUIVideoView<Background>(stream, background: background)
+        new.controller = controller
+        return new
+    }
+
+    /**
+     映像の描画を停止します。
+     */
+    private func videoStop(_ flag: Bool) -> SwiftUIVideoView<Background> {
+        if flag {
+            controller.videoView.stop()
+        } else if !controller.videoView.isRendering {
+            controller.videoView.start()
+        }
+        return self
+    }
+
+    /**
+     画面を背景ビューに切り替えます。
+     このメソッドは描画停止時のみ有効です。
+     */
+    public func videoClear(_ flag: Bool) -> SwiftUIVideoView<Background> {
+        if flag {
+            controller.videoView.clear()
+            controller.isCleared = true
+        }
+        return self
     }
 
     /// 映像のサイズの変更時に実行されるブロックを指定します。
@@ -90,6 +125,24 @@ public struct SwiftUIVideoView<Background>: View where Background: View {
         controller.videoView.handlers.onRender = perform
         return self
     }
+
+    /// 映像フレームの描画開始時に実行されるブロックを指定します。
+    public func videoOnStart(perform: @escaping () -> Void) -> SwiftUIVideoView<Background> {
+        controller.videoView.handlers.onStart = perform
+        return self
+    }
+
+    /// 映像フレームの描画停止時に実行されるブロックを指定します。
+    public func videoOnStop(perform: @escaping () -> Void) -> SwiftUIVideoView<Background> {
+        controller.videoView.handlers.onStop = perform
+        return self
+    }
+
+    /// 映像が backgroundView に切り替わったときに実行されるブロックを指定します。
+    public func videoOnClear(perform: @escaping () -> Void) -> SwiftUIVideoView<Background> {
+        controller.videoView.handlers.onClear = perform
+        return self
+    }
 }
 
 /*
@@ -99,11 +152,7 @@ private struct RepresentedVideoView: UIViewRepresentable {
     typealias UIViewType = VideoView
 
     @ObservedObject private var controller: VideoController
-
-    // 以下は SDK ユーザー側で制御される変数
-    /// 映像を停止するかを制御するフラグ
     @Binding private var isStop: Bool
-    /// backgroundView を表示するか制御するフラグ
     @Binding private var isClear: Bool
 
     public init(_ controller: VideoController, isStop: Binding<Bool>, isClear: Binding<Bool>) {
@@ -125,11 +174,6 @@ private struct RepresentedVideoView: UIViewRepresentable {
         // VideoView.clear() はVideoView.stop() が実行されたあとにのみ実行可能になる
         uiView = clear(stop(uiView))
         controller.stream?.videoRenderer = uiView
-    }
-
-    // 現在 VideoView が表示している映像の元々のフレームサイズを返します。
-    public var currentVideoFrameSize: CGSize? {
-        controller.videoView.currentVideoFrameSize
     }
 
     /**
@@ -156,12 +200,14 @@ private struct RepresentedVideoView: UIViewRepresentable {
     }
 }
 
-public class VideoController: ObservableObject {
+class VideoController: ObservableObject {
     var stream: MediaStream?
 
     // init() で VideoView を生成すると次のエラーが出るので、生成のタイミングを遅らせておく
     // Failed to bind EAGLDrawable: <CAEAGLLayer: 0x********> to GL_RENDERBUFFER 1
     lazy var videoView = VideoView()
+
+    @Published var isCleared: Bool = false
 
     init(stream: MediaStream?) {
         self.stream = stream
