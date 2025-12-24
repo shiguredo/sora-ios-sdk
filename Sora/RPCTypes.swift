c8fe19e7f0ec5057c44b8c6159fd9f5023f060c5
zztkm <33755694+zztkm@users.noreply.github.com>
2025-12-24 17:01:06 +0900

    RPCJSONValue をやめる

--- Sora/RPCTypes.swift
+++ Sora/RPCTypes.swift
@@ -47,123 +47,6 @@ public protocol RPCMethodProtocol {
   static var name: String { get }
 }
 
-public enum RPCJSONValue: Codable {
-  case object([String: RPCJSONValue])
-  case array([RPCJSONValue])
-  case string(String)
-  case number(Double)
-  case bool(Bool)
-  case null
-
-  public init?(any value: Any) {
-    if let dictionary = value as? [String: Any] {
-      var mapped: [String: RPCJSONValue] = [:]
-      for (key, item) in dictionary {
-        guard let jsonValue = RPCJSONValue(any: item) else {
-          return nil
-        }
-        mapped[key] = jsonValue
-      }
-      self = .object(mapped)
-      return
-    }
-    if let array = value as? [Any] {
-      let mapped = array.compactMap { RPCJSONValue(any: $0) }
-      guard mapped.count == array.count else {
-        return nil
-      }
-      self = .array(mapped)
-      return
-    }
-    if let string = value as? String {
-      self = .string(string)
-      return
-    }
-    if let bool = value as? Bool {
-      self = .bool(bool)
-      return
-    }
-    if let number = value as? NSNumber {
-      if CFGetTypeID(number) == CFBooleanGetTypeID() {
-        self = .bool(number.boolValue)
-      } else {
-        self = .number(number.doubleValue)
-      }
-      return
-    }
-    if value is NSNull {
-      self = .null
-      return
-    }
-    return nil
-  }
-
-  public init(from decoder: Decoder) throws {
-    if let container = try? decoder.container(keyedBy: DynamicCodingKeys.self) {
-      var object: [String: RPCJSONValue] = [:]
-      for key in container.allKeys {
-        object[key.stringValue] = try container.decode(RPCJSONValue.self, forKey: key)
-      }
-      self = .object(object)
-      return
-    }
-    if var container = try? decoder.unkeyedContainer() {
-      var array: [RPCJSONValue] = []
-      while !container.isAtEnd {
-        array.append(try container.decode(RPCJSONValue.self))
-      }
-      self = .array(array)
-      return
-    }
-    let container = try decoder.singleValueContainer()
-    if container.decodeNil() {
-      self = .null
-      return
-    }
-    if let bool = try? container.decode(Bool.self) {
-      self = .bool(bool)
-      return
-    }
-    if let number = try? container.decode(Double.self) {
-      self = .number(number)
-      return
-    }
-    if let string = try? container.decode(String.self) {
-      self = .string(string)
-      return
-    }
-    throw DecodingError.dataCorruptedError(
-      in: container, debugDescription: "RPCJSONValue のデコードに失敗しました")
-  }
-
-  public func encode(to encoder: Encoder) throws {
-    switch self {
-    case .object(let object):
-      var container = encoder.container(keyedBy: DynamicCodingKeys.self)
-      for (key, value) in object {
-        try container.encode(value, forKey: DynamicCodingKeys(stringValue: key))
-      }
-    case .array(let array):
-      var container = encoder.unkeyedContainer()
-      for value in array {
-        try container.encode(value)
-      }
-    case .string(let string):
-      var container = encoder.singleValueContainer()
-      try container.encode(string)
-    case .number(let number):
-      var container = encoder.singleValueContainer()
-      try container.encode(number)
-    case .bool(let value):
-      var container = encoder.singleValueContainer()
-      try container.encode(value)
-    case .null:
-      var container = encoder.singleValueContainer()
-      try container.encodeNil()
-    }
-  }
-}
-
 public struct RequestSimulcastRidParams: Encodable {
   public let rid: String
   public let senderConnectionId: String?
@@ -213,22 +96,22 @@ public struct ResetSpotlightRidParams: Encodable {
   }
 }
 
-public struct PutSignalingNotifyMetadataParams: Encodable {
-  public let metadata: [String: RPCJSONValue]
+public struct PutSignalingNotifyMetadataParams<Metadata: Encodable>: Encodable {
+  public let metadata: Metadata
   public let push: Bool?
 
-  public init(metadata: [String: RPCJSONValue], push: Bool? = nil) {
+  public init(metadata: Metadata, push: Bool? = nil) {
     self.metadata = metadata
     self.push = push
   }
 }
 
-public struct PutSignalingNotifyMetadataItemParams: Encodable {
+public struct PutSignalingNotifyMetadataItemParams<Value: Encodable>: Encodable {
   public let key: String
-  public let value: RPCJSONValue
+  public let value: Value
   public let push: Bool?
 
-  public init(key: String, value: RPCJSONValue, push: Bool? = nil) {
+  public init(key: String, value: Value, push: Bool? = nil) {
     self.key = key
     self.value = value
     self.push = push
@@ -301,9 +184,6 @@ public struct ResetSpotlightRidResult: Decodable {
   }
 }
 
-public typealias PutSignalingNotifyMetadataResult = [String: RPCJSONValue]
-public typealias PutSignalingNotifyMetadataItemResult = [String: RPCJSONValue]
-
 public enum RequestSimulcastRid: RPCMethodProtocol {
   public typealias Params = RequestSimulcastRidParams
   public typealias Result = RequestSimulcastRidResult
@@ -322,27 +202,16 @@ public enum ResetSpotlightRid: RPCMethodProtocol {
   public static let name = "2025.2.0/ResetSpotlightRid"
 }
 
-public enum PutSignalingNotifyMetadata: RPCMethodProtocol {
-  public typealias Params = PutSignalingNotifyMetadataParams
-  public typealias Result = PutSignalingNotifyMetadataResult
+public enum PutSignalingNotifyMetadata<Metadata: Codable>: RPCMethodProtocol {
+  public typealias Params = PutSignalingNotifyMetadataParams<Metadata>
+  public typealias Result = Metadata
   public static let name = "2025.2.0/PutSignalingNotifyMetadata"
 }
 
-public enum PutSignalingNotifyMetadataItem: RPCMethodProtocol {
-  public typealias Params = PutSignalingNotifyMetadataItemParams
-  public typealias Result = PutSignalingNotifyMetadataItemResult
+public enum PutSignalingNotifyMetadataItem<Metadata: Decodable, Value: Encodable>:
+  RPCMethodProtocol
+{
+  public typealias Params = PutSignalingNotifyMetadataItemParams<Value>
+  public typealias Result = Metadata
   public static let name = "2025.2.0/PutSignalingNotifyMetadataItem"
-}
-
-private struct DynamicCodingKeys: CodingKey {
-  let stringValue: String
-  let intValue: Int? = nil
-
-  init?(intValue: Int) {
-    return nil
-  }
-
-  init(stringValue: String) {
-    self.stringValue = stringValue
-  }
 }
