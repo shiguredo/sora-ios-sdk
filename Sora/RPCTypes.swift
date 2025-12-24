0a7193505ba86d9cc17b0786028f899a5aaf3ee3
zztkm <33755694+zztkm@users.noreply.github.com>
2025-12-24 14:54:27 +0900

    type 更新

--- Sora/RPCTypes.swift
+++ Sora/RPCTypes.swift
@@ -1,46 +1,5 @@
 import Foundation
 
-/// RPC メソッドを表す。
-public enum RPCMethod: Equatable {
-  case requestSimulcastRid
-  case requestSpotlightRid
-  case resetSpotlightRid
-  case putSignalingNotifyMetadata
-  case putSignalingNotifyMetadataItem
-
-  public init?(_ rawValue: String) {
-    switch rawValue {
-    case "2025.2.0/RequestSimulcastRid":
-      self = .requestSimulcastRid
-    case "2025.2.0/RequestSpotlightRid":
-      self = .requestSpotlightRid
-    case "2025.2.0/ResetSpotlightRid":
-      self = .resetSpotlightRid
-    case "2025.2.0/PutSignalingNotifyMetadata":
-      self = .putSignalingNotifyMetadata
-    case "2025.2.0/PutSignalingNotifyMetadataItem":
-      self = .putSignalingNotifyMetadataItem
-    default:
-      return nil
-    }
-  }
-
-  public var rawValue: String {
-    switch self {
-    case .requestSimulcastRid:
-      return "2025.2.0/RequestSimulcastRid"
-    case .requestSpotlightRid:
-      return "2025.2.0/RequestSpotlightRid"
-    case .resetSpotlightRid:
-      return "2025.2.0/ResetSpotlightRid"
-    case .putSignalingNotifyMetadata:
-      return "2025.2.0/PutSignalingNotifyMetadata"
-    case .putSignalingNotifyMetadataItem:
-      return "2025.2.0/PutSignalingNotifyMetadataItem"
-    }
-  }
-}
-
 public protocol RPCMethodProtocol {
   associatedtype Params: Encodable
   associatedtype Result: Decodable
@@ -235,11 +194,74 @@ public struct PutSignalingNotifyMetadataItemParams: Encodable {
   }
 }
 
-public typealias RequestSimulcastRidResult = RPCJSONValue
-public typealias RequestSpotlightRidResult = RPCJSONValue
-public typealias ResetSpotlightRidResult = RPCJSONValue
-public typealias PutSignalingNotifyMetadataResult = RPCJSONValue
-public typealias PutSignalingNotifyMetadataItemResult = RPCJSONValue
+public struct RequestSimulcastRidResult: Decodable {
+  public let channelId: String
+  public let receiverConnectionId: String
+  public let rid: String
+  public let senderConnectionId: String?
+
+  public init(
+    channelId: String,
+    receiverConnectionId: String,
+    rid: String,
+    senderConnectionId: String?
+  ) {
+    self.channelId = channelId
+    self.receiverConnectionId = receiverConnectionId
+    self.rid = rid
+    self.senderConnectionId = senderConnectionId
+  }
+
+  enum CodingKeys: String, CodingKey {
+    case channelId = "channel_id"
+    case receiverConnectionId = "receiver_connection_id"
+    case rid
+    case senderConnectionId = "sender_connection_id"
+  }
+}
+public struct RequestSpotlightRidResult: Decodable {
+  public let channelId: String
+  public let recvConnectionId: String
+  public let spotlightFocusRid: String
+  public let spotlightUnfocusRid: String
+
+  public init(
+    channelId: String,
+    recvConnectionId: String,
+    spotlightFocusRid: String,
+    spotlightUnfocusRid: String
+  ) {
+    self.channelId = channelId
+    self.recvConnectionId = recvConnectionId
+    self.spotlightFocusRid = spotlightFocusRid
+    self.spotlightUnfocusRid = spotlightUnfocusRid
+  }
+
+  enum CodingKeys: String, CodingKey {
+    case channelId = "channel_id"
+    case recvConnectionId = "recv_connection_id"
+    case spotlightFocusRid = "spotlight_focus_rid"
+    case spotlightUnfocusRid = "spotlight_unfocus_rid"
+  }
+}
+
+public struct ResetSpotlightRidResult: Decodable {
+  public let channelId: String
+  public let recvConnectionId: String
+
+  public init(channelId: String, recvConnectionId: String) {
+    self.channelId = channelId
+    self.recvConnectionId = recvConnectionId
+  }
+
+  enum CodingKeys: String, CodingKey {
+    case channelId = "channel_id"
+    case recvConnectionId = "recv_connection_id"
+  }
+}
+
+public typealias PutSignalingNotifyMetadataResult = [String: RPCJSONValue]
+public typealias PutSignalingNotifyMetadataItemResult = [String: RPCJSONValue]
 
 public enum RequestSimulcastRid: RPCMethodProtocol {
   public typealias Params = RequestSimulcastRidParams
