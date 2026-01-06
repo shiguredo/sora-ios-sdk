aba77509e770273d6aa70d756dc0d785f087c776
zztkm <33755694+zztkm@users.noreply.github.com>
2026-01-06 12:57:08 +0900

    format

--- SoraTests/SignalingOfferTests.swift
+++ SoraTests/SignalingOfferTests.swift
@@ -15,25 +15,25 @@ class SignalingOfferTests: XCTestCase {
       let json: String
       if simulcastRpcRids.isEmpty {
         json = """
-        {
-          "type": "offer",
-          "client_id": "client123",
-          "connection_id": "conn123",
-          "sdp": "v=0\\r\\no=- 1 1 IN IP4 127.0.0.1\\r\\n",
-          "simulcast_rpc_rids": []
-        }
-        """
+          {
+            "type": "offer",
+            "client_id": "client123",
+            "connection_id": "conn123",
+            "sdp": "v=0\\r\\no=- 1 1 IN IP4 127.0.0.1\\r\\n",
+            "simulcast_rpc_rids": []
+          }
+          """
       } else {
         let ridsJson = simulcastRpcRids.map { "\"\($0)\"" }.joined(separator: ", ")
         json = """
-        {
-          "type": "offer",
-          "client_id": "client123",
-          "connection_id": "conn123",
-          "sdp": "v=0\\r\\no=- 1 1 IN IP4 127.0.0.1\\r\\n",
-          "simulcast_rpc_rids": [\(ridsJson)]
-        }
-        """
+          {
+            "type": "offer",
+            "client_id": "client123",
+            "connection_id": "conn123",
+            "sdp": "v=0\\r\\no=- 1 1 IN IP4 127.0.0.1\\r\\n",
+            "simulcast_rpc_rids": [\(ridsJson)]
+          }
+          """
       }
 
       let data = json.data(using: .utf8)!
@@ -50,13 +50,13 @@ class SignalingOfferTests: XCTestCase {
 
   func testDecodeSimulcastRpcRidsNotPresent() throws {
     let json = """
-    {
-      "type": "offer",
-      "client_id": "client123",
-      "connection_id": "conn123",
-      "sdp": "v=0\\r\\no=- 1 1 IN IP4 127.0.0.1\\r\\n"
-    }
-    """
+      {
+        "type": "offer",
+        "client_id": "client123",
+        "connection_id": "conn123",
+        "sdp": "v=0\\r\\no=- 1 1 IN IP4 127.0.0.1\\r\\n"
+      }
+      """
     let data = json.data(using: .utf8)!
     let decoder = JSONDecoder()
     let offer = try decoder.decode(SignalingOffer.self, from: data)
