efba15c9cde82b34835079b10f999e08b6666a8d
zztkm <33755694+zztkm@users.noreply.github.com>
2026-01-08 12:03:59 +0900

    [canary] Update PackageInfo.swift version to 2026.1.0-canary.2

--- Sora/PackageInfo.swift
+++ Sora/PackageInfo.swift
@@ -1,7 +1,7 @@
 /// :nodoc:
 public enum SDKInfo {
   // Sora iOS SDK のバージョンを定義する
-  public static let version = "2026.1.0-canary.1"
+  public static let version = "2026.1.0-canary.2"
 }
 
 /// WebRTC フレームワークの情報を表します。
