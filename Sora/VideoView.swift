32fa0a549be04c04435fec45fc1d99ca223b430e
zztkm <33755694+zztkm@users.noreply.github.com>
2026-03-12 10:17:26 +0900

    VideoView のスレッドチェックは追加しない

--- Sora/VideoView.swift
+++ Sora/VideoView.swift
@@ -167,13 +167,7 @@ public class VideoView: UIView {
   /// 映像フレームの描画を停止します。
   /// 描画の停止中は ``render(videoFrame:)`` が実行されません。
   public func stop() {
-    if Thread.isMainThread {
-      isRendering = false
-    } else {
-      DispatchQueue.main.async {
-        self.isRendering = false
-      }
-    }
+    isRendering = false
   }
 
   /// デバッグモードを有効にします。
