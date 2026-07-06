3a8070c708bfca40a282c8f6bd189da20a939ec5
t-miya <tatumiya008005@gmail.com>
2026-07-06 14:59:46 +0900

    0008 videoH265Params にコメント追加

--- Sora/Configuration.swift
+++ Sora/Configuration.swift
@@ -239,6 +239,7 @@ public struct Configuration {
   public var videoH264Params: Encodable?
 
   /// H265 向け映像コーデックパラメーター
+  /// Sora 側含め実験的機能の段階です。
   public var videoH265Params: Encodable?
 
   // MARK: - イベントハンドラ
